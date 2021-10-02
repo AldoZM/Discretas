a22965acbe01588d793e0acd64dc2704c53aa0d2
AldoZM <aldo-298@hotmail.com>
2021-10-01 21:36:29 -0500

    Apuntes de conjuntos

---
+++ Conjuntos.py
@@ -0,0 +1,18 @@
+#Crear un conjunto en Python    
+
+#En python los conjuntos se representan mediante la estructura "set()"
+
+#Definicion de un conjunto 
+'''
+vocales = {'a','e','i','o','u'}
+print(vocales)
+'''
+
+
+#usas add() para agregar algun elemento al conjunto 
+
+#usar update() para ue funcione como un union 
+
+#Elminar algun elemento de un conjunto "remove()"
+
+#Vaciar un conjunto clear()
